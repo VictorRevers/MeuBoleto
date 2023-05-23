8c6401f0e10650b123a6de456f2a873c83836c70
VictorRevers <victorrkasnowski@gmail.com>
2023-05-23 13:34:45 -0300

    timer menu

--- src/main/java/System/Main.java
+++ src/main/java/System/Main.java
@@ -5,6 +5,8 @@ import com.sendmail.SendMail;
 import configsec.ConfigSec;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class Main {
@@ -13,13 +15,36 @@ public class Main {
         int today = date.getDate();
         ConfigSec config = new ConfigSec();
         int option = 0;
+        Timer timer = new Timer();
+        
+        TimerTask task = new TimerTask(){
+            public void run(){
+                String[] auth = config.readConfig();
+                SendMail sendMail = new SendMail();
+        
+                if(auth[0] != null && auth[1] != null && auth[2] != null){
+                    sendMail.setFrom(auth[0]);
+                    sendMail.setPass(auth[1]);
+                    sendMail.setTo(auth[2]);
+            
+                    sendMail.sendEmail();
+                }else{
+                    System.out.println("VOCÊ PRECISA CONFIGURAR AS INFORMAÇÕES DE ENVIO E AUTENTICAÇÃO!");
+                }
+                System.exit(0);
+            }
+        };
+        
+        int limitTime = 5000;
+        timer.schedule(task, limitTime);
+        
         
         Scanner sc = new Scanner(System.in);
         System.out.println("***INSIRA 1 PARA CONFIGURAR AS INFORMAÇÕES DE ENVIO E AUTENTICAÇÃO!***");
         System.out.println("***INSIRA 2 PARA CADASTRAR NOVO BOLETO!***");
         
         System.out.println("->: ");  option = sc.nextInt();
-        
+        timer.cancel();
         
         
         switch(option){
@@ -40,31 +65,16 @@ public class Main {
             break;
             case 2: 
                 Scanner sc3 = new Scanner(System.in);
-                System.out.println("INSIRA O BOLETO NO SEGUINTE FORMATO: NOME DO BOLETO, DIA VENCIMENTO(cartão, 10)");
+                System.out.println("INSIRA O BOLETO NO SEGUINTE FORMATO: NOME DO BOLETO, DIA VENCIMENTO Ex:(cartão, 10)");
             break;
+            default: 
+                System.out.println("Opção inexistente!");
         }
         
         
-        if(option == 1){
-            
-        }
         
         sc.close();
-   
-        String[] auth = config.readConfig();
-        SendMail sendMail = new SendMail();
-        
-        if(auth[0] != null && auth[1] != null && auth[2] != null){
-            sendMail.setFrom(auth[0]);
-            sendMail.setPass(auth[1]);
-            sendMail.setTo(auth[2]);
-            
-            sendMail.sendEmail();
-        }else{
-            System.out.println("VOCÊ PRECISA CONFIGURAR AS INFORMAÇÕES DE ENVIO E AUTENTICAÇÃO!");
-        }
-                    
-                  
+                                      
        System.exit(0);
     }
 }
